4805a3993010bee67227191b84f2d9d8dd2df428
Phillip Lee <20524533+ph0ph0@users.noreply.github.com>
2021-08-24 15:05:30 +0100

    rough finish of stripe payment

--- package/index.js
+++ package/index.js
@@ -1,12 +1,47 @@
 const stripe = require("stripe")(process.env.stripeKey);
 
 exports.handler = async (event, context) => {
-  console.log(`Event: ${event}`);
-  const response = {
+  console.log(`Event: ${JSON.stringify(event)}`);
+
+  const token = event.token.token;
+
+  console.log(`Token: ${JSON.stringify(token)}`);
+
+  const headers = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Headers":
+      "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+    "Access-Control-Allow-Methods": "OPTIONS,POST",
+    "Access-Control-Allow-Credentials": true,
+    "Access-Control-Allow-Origin": "*",
+    "X-Requested-With": "*",
+  };
+
+  //   Dollars
+  const partialOptimiserPrice = 4.99;
+
+  let response = {
     statusCode: 200,
-    body: JSON.stringify("Hello there mate!"),
     headers: headers,
   };
+
+  try {
+    await stripe.charges.create({
+      amount: partialOptimiserPrice,
+      currency: "usd",
+      source: token.id,
+      description: `Payment for partial optimiser vending machine`,
+      metadta: {
+        vendingMachine: "PO",
+      },
+    });
+    console.log(`Successfully charged credit card`);
+    response["body"] = "Payment Success";
+  } catch (error) {
+    console.log(`Error charging credit card: ${error}`);
+    response["body"] = "Payment failure";
+    response["statusCode"] = 500;
+  }
 
   return response;
 };
